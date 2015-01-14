d6afd44fca95627b9533dc944e75da2214d3b4bd
Armin Engesser <armin.engesser@gmail.com>
2015-01-14 22:21:19 +0100

    - added some icons

---
+++ app/src/main/java/de/htwg/mc/irdroid/model/CommandType.java
@@ -0,0 +1,10 @@
+package de.htwg.mc.irdroid.model;
+
+/**
+ * Created by armin on 14/01/15.
+ *
+ * type of specific command
+ */
+public enum CommandType {
+    power, volumeUp, volumeDown, channelUp, channelDown, digits, other
+}
